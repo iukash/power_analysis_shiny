217c68a64b11a3a542ec9a01fdf0dedf2237c9e7
iukash <lukash.a.a@yandex.ru>
2020-10-14 22:00:36 +0300

    t test

--- power_analysys_shiny/server.R
+++ power_analysys_shiny/server.R
@@ -1,5 +1,55 @@
 library(shiny)
 
-shinyServer(function(input, output) {
-
+shinyServer(function(input, output, session) {
+  observe({
+    if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
+                                                                                           size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
+    if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
+                                                                                           size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
+    if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
+                                                                                           size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
+    if(input$select_test == 1) {
+      input.n <- input$slider_n; input.d <- input$slider_effect; 
+      input.p_value <- input$slider_p_value;input.power <- input$slider_power; 
+      input.type <- ifelse(input$select_type==1,{"paired"},{"two.sample"})
+      input.alternative <- ifelse(input$select_alternative==1, {"two.sided"}, 
+                                  ifelse(input$select_alternative==2,{"greater"},{"less"}))
+      
+      if(input$radio == 1){
+        shinyjs::disable('slider_power'); shinyjs::enable('slider_p_value'); 
+        shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
+        text_rezult <- "мощность"
+        rez_test <- pwr.t.test(n = input.n, d = input.d, sig.level = input.p_value, power = NULL,
+                               type = input.type, alternative = input.alternative)
+        text_rezult <- paste(text_rezult,round(rez_test$power,3))
+      }
+      if(input$radio == 2){
+        shinyjs::enable('slider_power'); shinyjs::disable('slider_p_value'); 
+        shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
+        text_rezult <- "достоверность"
+        rez_test <- pwr.t.test(n = input.n, d = input.d, sig.level = NULL, power = input.power,
+                               type = input.type, alternative = input.alternative)
+        text_rezult <- paste(text_rezult,round(rez_test$sig.level,3))
+      }
+      if(input$radio == 3){
+        shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
+        shinyjs::disable('slider_effect'); shinyjs::enable('slider_n');
+        text_rezult <- "величина эффекта"
+        rez_test <- pwr.t.test(n = input.n, d = NULL, sig.level = input.p_value, power = input.power,
+                               type = input.type, alternative = input.alternative)
+        text_rezult <- paste(text_rezult,round(rez_test$d,3))
+      }
+      if(input$radio == 4){
+        shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
+        shinyjs::enable('slider_effect'); shinyjs::disable('slider_n');
+        text_rezult <- "количество необходимых исследований"
+        rez_test <- pwr.t.test(n = NULL, d = input.d, sig.level = input.p_value, power = input.power,
+                               type = input.type, alternative = input.alternative)
+        text_rezult <- paste(text_rezult,as.integer(round(rez_test$n,0)))
+      }
+    }
+    if(input$select_test == 2) {text_rezult <- "тест Хи-квадрат в разработке"}
+    if(input$select_test == 3) {text_rezult <- "тест корреляции Пирсона в разработке"}
+    output$output_text <- renderText({text_rezult})
+  })
 })
--- power_analysys_shiny/ui.R
+++ power_analysys_shiny/ui.R
@@ -1,55 +1,57 @@
 library(shiny)
+library(shinyjs)
+library(pwr)
 
 shinyUI(fluidPage(
+  useShinyjs(),
   titlePanel(h2("Расчет количества/мощности/достоверности/количественного эффекта исследований", align = "center")),
-  sidebarLayout(
-    sidebarPanel("Возможно здесь нужна краткая инструкция по применению типо выбери 
-                     3 параметра нажми расчет получишь четвертый"),
-    mainPanel(
-      selectInput("select_test", label = h3("Выберете критерий"), choices = list(
-        "Тест Стьюдента" = 1,"Тест хи-квадрат" = 2,"Тест корреляции" = 3)),
-      h3("Выберите значения параметров"),
-      fluidRow(
-        column(5, 
-               sliderInput("slider_power", label = h4 ("Мощность"), min = 0, max = 1, value = 0.9, step = 0.05)
-        ),
-        column(5, 
-               sliderInput("slider_p_value", label = h4 ("Достоверность"), min = 0, max = 1, value = 0.05, step = 0.05)
-        )
+  mainPanel(
+    selectInput("select_test", label = h3("Выберете критерий"), choices = list(
+      "Тест Стьюдента" = 1,"Тест хи-квадрат" = 2,"Тест корреляции Пирсона" = 3)),
+    h3("Выберите значения параметров"),
+    fluidRow(
+      column(6, 
+             sliderInput("slider_power", label = h4 ("Мощность"), min = 0, max = 1, value = 0.9, step = 0.05)
       ),
-      fluidRow(
-        column(5, 
-               sliderInput("slider_effect", label = h4 ("Величина эффекта"), min = 0, max = 1, value = 0.9, step = 0.05)
-        ),
-        column(5, 
-               sliderInput("slider_n", label = h4 ("Количество исследований"), min = 1, max = 200, value = 1, step = 1)
-        )
+      column(6, 
+             sliderInput("slider_p_value", label = h4 ("Достоверность"), min = 0, max = 1, value = 0.05, step = 0.05)
+      )
+    ),
+    fluidRow(
+      column(6, h4 ("Величина эффекта"),
+             radioButtons("radio_cohen", label = (""), inline = TRUE,
+                          choices = list("Слабый" = 1, "Средний" = 2,"Сильный" = 3), selected = 2),
+             sliderInput("slider_effect", label = (""), min = 0, max = 1, value = 0.5, step = 0.05)
       ),
-      radioButtons("radio", label = h3("Выберите рассчитываемый параметр"), inline = TRUE,
-                   choices = list("Мощность" = 1, "Достоверность" = 2,"Величина эффекта" = 3,
-                                  "Количество исследований" = 4), selected = 4
+      column(6, 
+             sliderInput("slider_n", label = h4 ("Количество исследований"), min = 1, max = 200, value = 44, step = 1)
+      )
+    ),
+    radioButtons("radio", label = h3("Выберите рассчитываемый параметр"), inline = TRUE,
+                 choices = list("Мощность" = 1, "Достоверность" = 2,"Величина эффекта" = 3,
+                                "Количество исследований" = 4), selected = 4
+    ),
+    #br(),
+    h4("Выберите дополнительные параметры для выбранного теста"),
+    fluidRow(
+      column(6, 
+             selectInput("select_type", label = h4 ("Тип"), choices = list(
+               "Зависимые выборки" = 1,"Независимые выборки" = 2))
       ),
-      br(),
-      h4("Выберите дополнительные параметры для выбранного теста"),
-      fluidRow(
-        column(5, 
-               selectInput("select_type", label = h4 ("Тип"), choices = list(
-                 "Зависимые выборки" = 1,"Независимые выборки" = 2))
-        ),
-        column(5, 
-               selectInput("select_alternative", label = h4 ("Направление"), choices = list(
-                 "Двухсторонний" = 1,"Односторонний возрастающий" = 2,"Односторонний убывающий" = 3))
-        )
+      column(6, 
+             selectInput("select_alternative", label = h4 ("Направление"), choices = list(
+               "Двухсторонний" = 1,"Односторонний возрастающий" = 2,"Односторонний убывающий" = 3))
+      )
+    ),
+    fluidRow(
+      column(6, 
+             h4("РЕЗУЛЬТАТ РАСЧЕТА"),
       ),
-      fluidRow(
-        column(5, 
-               textOutput("output")
-        ),
-        column(5, 
-               actionButton("action", label = h4 ("Расчет"))
-        )
-      ),
-    )
-  )
+      column(6, 
+             textOutput("output_text")
+      )
+    ),
+    
+  )  
   )
 )
