8c711ab01ef7b800f234d402b80b823be76face8
iukash <lukash.a.a@yandex.ru>
2020-10-15 21:20:43 +0300

    parametric release

--- power_analysys_shiny/server.R
+++ power_analysys_shiny/server.R
@@ -2,54 +2,195 @@ library(shiny)
 
 shinyServer(function(input, output, session) {
   observe({
-    if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
-                                                                                           size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
-    if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
-                                                                                           size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
-    if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", value = cohen.ES(test = "t", 
-                                                                                           size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
-    if(input$select_test == 1) {
-      input.n <- input$slider_n; input.d <- input$slider_effect; 
-      input.p_value <- input$slider_p_value;input.power <- input$slider_power; 
-      input.type <- ifelse(input$select_type==1,{"paired"},{"two.sample"})
-      input.alternative <- ifelse(input$select_alternative==1, {"two.sided"}, 
-                                  ifelse(input$select_alternative==2,{"greater"},{"less"}))
+    #Выбор способа расчета
+    if(input$select_parametric == 1)
+    {
+    #Скрытие элементов страницы моделирование
+    shinyjs::hide(id = "output_message");
       
-      if(input$radio == 1){
-        shinyjs::disable('slider_power'); shinyjs::enable('slider_p_value'); 
-        shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
-        text_rezult <- "мощность"
+    #Отображение элементов страницы расчета
+    shinyjs::show(id = "select_test"); shinyjs::show(id = "slider_power"); shinyjs::show(id = "slider_p_value");
+    shinyjs::show(id = "radio_cohen"); shinyjs::show(id = "slider_effect"); shinyjs::show(id = "slider_n");
+    shinyjs::show(id = "radio"); shinyjs::show(id = "select_type"); shinyjs::show(id = "select_alternative");
+    shinyjs::show(id = "slider_df"); shinyjs::show(id = "slider_k"); shinyjs::show(id = "output_select_dop");
+    shinyjs::show(id = "output_effect_value"); shinyjs::show(id = "output_slider_value"); shinyjs::show(id = "output_text");
+    
+    #Привязка текста к элементам
+    output$title_my <- renderText({"Математический расчет количества/мощности/достоверности/количественного эффекта исследований"})
+    output$output_select_dop <- renderText({"Выберите дополнительные параметры для выбранного теста"})
+    output$output_effect_value <- renderText({"Величина эффекта"})
+    output$output_slider_value <- renderText({"Выберите значения параметров"})
+    
+    #получение значений со слайдеров количества/мощности/достоверности/количественного эффекта
+    input.n <- input$slider_n; input.d <- input$slider_effect; 
+    input.p_value <- input$slider_p_value;input.power <- input$slider_power; 
+    input.type <- ifelse(input$select_type==1,{"paired"},{"two.sample"})
+    input.alternative <- ifelse(input$select_alternative==1, {"two.sided"}, 
+                                ifelse(input$select_alternative==2,{"greater"},{"less"}))
+    input.slider_df <- input$slider_df
+    input.slider_k <- input$slider_k
+    
+    #активность слайдеров количества/мощности/достоверности/количественного эффекта в зависимости от выбранного
+    #рассчитываемого параметра
+    if(input$radio == 1){
+      shinyjs::disable('slider_power'); shinyjs::enable('slider_p_value'); 
+      shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
+      text_rezult <- "мощность"
+      
+      #применение выбранного теста
+      if (input$select_test == 1) {
         rez_test <- pwr.t.test(n = input.n, d = input.d, sig.level = input.p_value, power = NULL,
-                               type = input.type, alternative = input.alternative)
-        text_rezult <- paste(text_rezult,round(rez_test$power,3))
-      }
-      if(input$radio == 2){
-        shinyjs::enable('slider_power'); shinyjs::disable('slider_p_value'); 
-        shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
-        text_rezult <- "достоверность"
+                               type = input.type, alternative = input.alternative)}
+      if (input$select_test == 2) {
+        rez_test <- pwr.chisq.test(w = input.d, N = input.n, df = input.slider_df, sig.level = input.p_value, power = NULL)}
+      if (input$select_test == 3) {
+        rez_test <- pwr.r.test(n = input.n, r = input.d, sig.level = input.p_value, power = NULL, 
+                               alternative = input.alternative)}
+      if (input$select_test == 4) {
+        rez_test <- pwr.anova.test(k = input.slider_k, n = input.n, f = input.d, sig.level = input.p_value, power = NULL)}
+      
+      text_rezult <- paste(text_rezult,round(rez_test$power,3))
+    }
+    if(input$radio == 2){
+      shinyjs::enable('slider_power'); shinyjs::disable('slider_p_value'); 
+      shinyjs::enable('slider_effect'); shinyjs::enable('slider_n');
+      text_rezult <- "достоверность"
+      
+      #применение выбранного теста
+      if (input$select_test == 1) {
         rez_test <- pwr.t.test(n = input.n, d = input.d, sig.level = NULL, power = input.power,
-                               type = input.type, alternative = input.alternative)
-        text_rezult <- paste(text_rezult,round(rez_test$sig.level,3))
-      }
-      if(input$radio == 3){
-        shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
-        shinyjs::disable('slider_effect'); shinyjs::enable('slider_n');
-        text_rezult <- "величина эффекта"
+                               type = input.type, alternative = input.alternative)}
+      if (input$select_test == 2) {
+        rez_test <- pwr.chisq.test(w = input.d, N = input.n, df = input.slider_df, sig.level = NULL, power = input.power)}
+      if (input$select_test == 3) {
+        rez_test <- pwr.r.test(n = input.n, r = input.d, sig.level = NULL, power = input.power, 
+                               alternative = input.alternative)}
+      if (input$select_test == 4) {
+        rez_test <- pwr.anova.test(k = input.slider_k, n = input.n, f = input.d, sig.level = NULL, power = input.power)}
+      
+      text_rezult <- paste(text_rezult,round(rez_test$sig.level,3))
+    }
+    if(input$radio == 3){
+      shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
+      shinyjs::disable('slider_effect'); shinyjs::enable('slider_n');
+      text_rezult <- "величина эффекта"
+      
+      #применение выбранного теста
+      if (input$select_test == 1) {
         rez_test <- pwr.t.test(n = input.n, d = NULL, sig.level = input.p_value, power = input.power,
                                type = input.type, alternative = input.alternative)
-        text_rezult <- paste(text_rezult,round(rez_test$d,3))
-      }
-      if(input$radio == 4){
-        shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
-        shinyjs::enable('slider_effect'); shinyjs::disable('slider_n');
-        text_rezult <- "количество необходимых исследований"
+        text_rezult <- paste(text_rezult,round(rez_test$d,3))}
+      if (input$select_test == 2) {
+        rez_test <- pwr.chisq.test(w = NULL, N = input.n, df = input.slider_df, sig.level = input.p_value, power = input.power)
+        text_rezult <- paste(text_rezult,round(rez_test$w,3))}
+      if (input$select_test == 3) {
+        rez_test <- pwr.r.test(n = input.n, r = NULL, sig.level = input.p_value, power = input.power, 
+                               alternative = input.alternative)
+        text_rezult <- paste(text_rezult,round(rez_test$r,3))}
+      if (input$select_test == 4) {
+        rez_test <- pwr.anova.test(k = input.slider_k, n = input.n, f = NULL, sig.level = input.p_value, power = input.power)
+        text_rezult <- paste(text_rezult,round(rez_test$f,3))}
+    }
+    if(input$radio == 4){
+      shinyjs::enable('slider_power'); shinyjs::enable('slider_p_value'); 
+      shinyjs::enable('slider_effect'); shinyjs::disable('slider_n');
+      text_rezult <- "количество необходимых исследований"
+      
+      #применение выбранного теста
+      if (input$select_test == 1) {
         rez_test <- pwr.t.test(n = NULL, d = input.d, sig.level = input.p_value, power = input.power,
                                type = input.type, alternative = input.alternative)
-        text_rezult <- paste(text_rezult,as.integer(round(rez_test$n,0)))
-      }
+        text_rezult <- paste(text_rezult,as.integer(round(rez_test$n,0)))}
+      if (input$select_test == 2) {
+        rez_test <- pwr.chisq.test(w = input.d, N = NULL, df = input.slider_df, sig.level = input.p_value, power = input.power)
+        text_rezult <- paste(text_rezult,as.integer(round(rez_test$N,0)))}
+      if (input$select_test == 3) {
+        rez_test <- pwr.r.test(n = NULL, r = input.d, sig.level = input.p_value, power = input.power, 
+                               alternative = input.alternative)
+        text_rezult <- paste(text_rezult,as.integer(round(rez_test$n,0)))}
+      if (input$select_test == 4) {
+        rez_test <- pwr.anova.test(k = input.slider_k, n = NULL, f = input.d, sig.level = input.p_value, power = input.power)
+        text_rezult <- paste(text_rezult,as.integer(round(rez_test$n,0)))}
     }
-    if(input$select_test == 2) {text_rezult <- "тест Хи-квадрат в разработке"}
-    if(input$select_test == 3) {text_rezult <- "тест корреляции Пирсона в разработке"}
+    
+    #ТЕСТ t критерий Стьюдента 
+    if(input$select_test == 1) {
+      #Отображение/скрытие дополнительных параметров для теста
+      shinyjs::show(id = "select_alternative"); shinyjs::show(id = "select_type")
+      shinyjs::hide(id = "slider_df"); shinyjs::hide(id = "slider_k")
+      
+      #Выбор величины Эффекта по Коэну
+      if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "t", size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "t", size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "t", size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
+    }
+    
+    #ТЕСТ хи-квадрат
+    if(input$select_test == 2) {
+      #Отображение/скрытие дополнительных параметров для теста
+      shinyjs::hide(id = "select_alternative"); shinyjs::hide(id = "select_type")
+      shinyjs::hide(id = "slider_k"); shinyjs::show(id = "slider_df")
+      
+      #Выбор величины Эффекта по Коэну
+      if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "chisq", size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "chisq", size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "chisq", size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
+    }
+    
+    #Тест корреляции
+    if(input$select_test == 3) {
+      #Отображение/скрытие дополнительных параметров для теста
+      shinyjs::hide(id = "select_type"); shinyjs::hide(id = "slider_k")
+      shinyjs::hide(id = "slider_df"); shinyjs::show(id = "select_alternative");
+      
+      #Выбор величины Эффекта по Коэну
+      if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "r", size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "r", size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "r", size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
+    }
+    
+    #Дисперсионный анализ anova
+    if(input$select_test == 4) {
+      #Отображение/скрытие дополнительных параметров для теста
+      shinyjs::hide(id = "select_alternative"); shinyjs::hide(id = "select_type")
+      shinyjs::hide(id = "slider_df"); shinyjs::show(id = "slider_k")
+      
+      #Выбор величины Эффекта по Коэну
+      if(input$radio_cohen==1) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "anov", size = "small")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==2) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "anov", size = "medium")$effect.size,min = 0, max = 1, step = 0.05)}
+      if(input$radio_cohen==3) {updateSliderInput(session, "slider_effect", 
+                                                  value = cohen.ES(test = "anov", size = "large")$effect.size,min = 0, max = 1, step = 0.05)}
+    }
+    
     output$output_text <- renderText({text_rezult})
+    }
+    else
+    {
+    #Скрытие элементов страницы расчета
+    shinyjs::hide(id = "select_test"); shinyjs::hide(id = "slider_power"); shinyjs::hide(id = "slider_p_value");
+    shinyjs::hide(id = "radio_cohen"); shinyjs::hide(id = "slider_effect"); shinyjs::hide(id = "slider_n");
+    shinyjs::hide(id = "radio"); shinyjs::hide(id = "select_type"); shinyjs::hide(id = "select_alternative");
+    shinyjs::hide(id = "slider_df"); shinyjs::hide(id = "slider_k"); shinyjs::hide(id = "output_select_dop");
+    shinyjs::hide(id = "output_effect_value"); shinyjs::hide(id = "output_slider_value"); shinyjs::hide(id = "output_text");
+    
+    #Отображение элементов страницы моделирования
+    shinyjs::show(id = "output_message");
+    
+    #Привязка текста к элементам
+    output$title_my <- renderText({"Прогнозирование количества исследований средствами математического моделирования"})
+    output$output_message <- renderText({"ПОКА НЕ СДЕЛАНО, НО СДЕЛАЮ!!!"})
+    }
+    
   })
 })
--- power_analysys_shiny/ui.R
+++ power_analysys_shiny/ui.R
@@ -4,11 +4,16 @@ library(pwr)
 
 shinyUI(fluidPage(
   useShinyjs(),
-  titlePanel(h2("Расчет количества/мощности/достоверности/количественного эффекта исследований", align = "center")),
-  mainPanel(
-    selectInput("select_test", label = h3("Выберете критерий"), choices = list(
-      "Тест Стьюдента" = 1,"Тест хи-квадрат" = 2,"Тест корреляции Пирсона" = 3)),
-    h3("Выберите значения параметров"),
+    h3(textOutput(outputId = "title_my"), align = "center"),
+  
+    #начало страницы расчета
+    radioButtons("select_parametric", label = (""), inline = TRUE, choices = list(
+                          "Расчет для параметрических критериев" = 1, 
+                          "Прогнозирование средствами моделирования" = 2), 
+              selected = 1),
+    selectInput("select_test", label = h3("Выберете критерий"), choices = list("Тест Стьюдента" = 1,
+                               "Тест хи-квадрат" = 2,"Тест корреляции" = 3,"Дисперсионный анализ anova" = 4)),
+    h3(textOutput("output_slider_value")),
     fluidRow(
       column(6, 
              sliderInput("slider_power", label = h4 ("Мощность"), min = 0, max = 1, value = 0.9, step = 0.05)
@@ -18,7 +23,7 @@ shinyUI(fluidPage(
       )
     ),
     fluidRow(
-      column(6, h4 ("Величина эффекта"),
+      column(6, h4(textOutput("output_effect_value")),
              radioButtons("radio_cohen", label = (""), inline = TRUE,
                           choices = list("Слабый" = 1, "Средний" = 2,"Сильный" = 3), selected = 2),
              sliderInput("slider_effect", label = (""), min = 0, max = 1, value = 0.5, step = 0.05)
@@ -31,8 +36,8 @@ shinyUI(fluidPage(
                  choices = list("Мощность" = 1, "Достоверность" = 2,"Величина эффекта" = 3,
                                 "Количество исследований" = 4), selected = 4
     ),
-    #br(),
-    h4("Выберите дополнительные параметры для выбранного теста"),
+
+    h4(textOutput("output_select_dop")),
     fluidRow(
       column(6, 
              selectInput("select_type", label = h4 ("Тип"), choices = list(
@@ -41,17 +46,16 @@ shinyUI(fluidPage(
       column(6, 
              selectInput("select_alternative", label = h4 ("Направление"), choices = list(
                "Двухсторонний" = 1,"Односторонний возрастающий" = 2,"Односторонний убывающий" = 3))
-      )
-    ),
-    fluidRow(
-      column(6, 
-             h4("РЕЗУЛЬТАТ РАСЧЕТА"),
       ),
       column(6, 
-             textOutput("output_text")
-      )
-    ),
-    
-  )  
+             sliderInput("slider_df", label = h4 ("Количество степеней свободы"), min = 1, max = 50, value = 1, step = 1)
+      ),
+      column(6, 
+             sliderInput("slider_k", label = h4 ("Количество групп"), min = 1, max = 20, value = 3, step = 1))
+  ),
+      h3(textOutput("output_text"), align = "center"),
+  
+  #начало страницы моделирование
+  h3(textOutput("output_message"), align = "center")
   )
 )
